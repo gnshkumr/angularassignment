d6875dc65e2e6ea060c193d4245ed70e939353fc
Ganesh <dgkreddy98@gmail.com>
2020-01-31 14:03:09 +0530

    Init Angular repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  todos=["Abc","efg","ijk"];
+
+  add(newtodo : string){
+
+  this.todos.push(newtodo);
+  }
+delete(todo){
+  this.todos=this.todos.filter(t => (t!==todo));
+}
+
+deleteall(){
+  this.todos=[];
+}
+update(msg,todo){
+  this.todos[this.todos.indexOf(todo)]=msg;
+}
+
 }
